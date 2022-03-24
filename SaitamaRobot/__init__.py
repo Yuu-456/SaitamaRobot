f653340e8baf49a156e3e1c8b75f6316b4782968
Yuu-456 <99785450+Yuu-456@users.noreply.github.com>
2022-03-24 14:35:02 +0900

    Update __init__.py

--- SaitamaRobot/__init__.py
+++ SaitamaRobot/__init__.py
@@ -92,7 +92,7 @@ if ENV:
         raise Exception("Your blacklisted chats list does not contain valid integers.")
 
 else:
-    from SaitamaRobot.config import Development as Config
+    from Anime X EZN.config import Development as Config
 
     TOKEN = Config.TOKEN
 
@@ -180,7 +180,7 @@ DEMONS = list(DEMONS)
 TIGERS = list(TIGERS)
 
 # Load at end to ensure all prev variables have been set
-from SaitamaRobot.modules.helper_funcs.handlers import (
+from Anime X EZN.modules.helper_funcs.handlers import (
     CustomCommandHandler,
     CustomMessageHandler,
     CustomRegexHandler,
